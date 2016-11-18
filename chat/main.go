25d47598e66d2601ffab55e33ba19a415d051798
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-11-18 22:13:05 +0100

    Slice Literal

--- chat/main.go
+++ chat/main.go
@@ -9,14 +9,7 @@ import (
 )
 
 func main() {
-	var blockedIPs []string
-
-	blockedIPs = append(blockedIPs, "192.168.0.16")
-	blockedIPs = append(blockedIPs, "192.168.0.17")
-	blockedIPs = append(blockedIPs, "192.168.0.18")
-	blockedIPs = append(blockedIPs, "192.168.0.19")
-	blockedIPs = append(blockedIPs, "192.168.0.20")
-
+	blockedIPs := []string{"192.168.0.19", "192.168.0.20"}
 	addToBlockedList(blockedIPs)
 }
 
